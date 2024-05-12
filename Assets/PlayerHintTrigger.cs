502217ac3b8949e95b3418a1a6377f237eef3940
AlSG <alex.gub2013@gmail.com>
2024-05-13 01:59:30 +0700

    Working on dynamic lozalization change

--- Assets/PlayerHintTrigger.cs
+++ Assets/PlayerHintTrigger.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
 
 public class PlayerHintTrigger : MonoBehaviour
 {
     [SerializeField] private string[] _text;
-    public string tetetest;
+    [SerializeField] private LocalizedString test;
 
     public static event System.Action<string> ShowingHint;
 
@@ -15,10 +17,10 @@ public class PlayerHintTrigger : MonoBehaviour
         {
             return;
         }
-
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
         foreach (var text in _text)
         {
-            ShowingHint?.Invoke(text);
+            ShowingHint?.Invoke(test.GetLocalizedString());
         }
 
         gameObject.GetComponent<Collider>().enabled = false;
--- Assets/Prefabs/Triggers/TestTrigger.cs
+++ Assets/Prefabs/Triggers/TestTrigger.cs
@@ -12,16 +12,17 @@ public class TestTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //test = new LocalizedString("LocalizationStringTable", "YoloEntry");
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
         if (other.CompareTag("Player"))
         {
-
+            test.GetLocalizedString();
+            string buba = test.GetLocalizedString(0);
 
             //var collection = LocalizationEditorSettings.GetStringTableCollection("LocalizationStringTable");
             //var entry = collection.SharedData.GetEntry("YoloEntry");
             //test = new LocalizedString(collection.SharedData.TableCollectionNameGuid, entry.Id);
             var translatedValue = LocalizationSettings.StringDatabase.GetLocalizedString("LocalizationStringTable", "YoloEntry");
-            Debug.Log(translatedValue);
+            Debug.Log(test);
         }
 
         
